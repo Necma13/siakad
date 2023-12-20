94b5e797d46c5d2cfd393c6182e44ff22019f985
Necma13 <145630799+Necma13@users.noreply.github.com>
2023-12-20 22:58:07 +0700

    Update login.php

--- login.php
+++ login.php
@@ -84,7 +84,7 @@ else{
           </div>
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit"  class="btn btn-primary btn-block">Sign In</button>
+            <button type="submit" name="tombol" class="btn btn-primary btn-block">Sign In</button>
           </div>
           <!-- /.col -->
         </div>
